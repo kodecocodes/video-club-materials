f693b1478d97feff08a02b81d481d29f21a7af65
Marcel Schnelle <marcelschnelle@aol.com>
2021-09-29 16:18:59 +0200

    Add project skeleton

---
+++ 99-final-project/Final/app/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+val javaVersion = JavaVersion.VERSION_11
+
+android {
+    compileSdk = 31
+
+    defaultConfig {
+        applicationId = "com.raywenderlich.android.club"
+        minSdk = 21
+        targetSdk = 31
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+    }
+    kotlinOptions {
+        jvmTarget = javaVersion.toString()
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("com.google.android.material:material:1.4.0")
+
+    testImplementation("junit:junit:4.13.2")
+}
